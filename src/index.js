6ec4f000d64fa9915eb2671e5b76753e4e0bd55a
Stefan Milovanovic <milovanovic.stefan@outlook.com>
2019-01-29 00:39:28 +0100

    feat(gis): completed almost

--- src/index.js
+++ src/index.js
@@ -7,24 +7,76 @@ import { feature } from './utils'
 var map = L.map('map').setView(new L.LatLng(44.241, 20.912), 9);
 
 
-var base = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var OpenStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+var baseLayers = {
+  "Open Street Map": OpenStreetMap,
+};
 
 const createLayer = create(map)("http://localhost:8080/geoserver/gis/wms")
-const getFeature = feature(map)("http://localhost:8080/geoserver/gis/wms")
 
-let activeLayer = createLayer("gis:gis_osm_places_a_free_1").addTo(map)
+let activeLayer = createLayer("gis:gis_osm_places_a_free_1").addTo(map);
+var markerPoints = L.layerGroup();
+
+feature(activeLayer.options.layers).then(response => {
+  L.geoJson(response, {
+    onEachFeature: function (feature, url) {
+      var popupOptions = { maxWidth: 250 };
+      url.bindPopup(
+        "<br><b>name: </b>" + feature.properties.name +
+        "<br><b>osm_id: </b>" + feature.properties.osm_id
+        , popupOptions);
+    }, pointToLayer: function (feature, latlng) {
+      return getMarker(feature, latlng);
+    }
+  }).addTo(markerPoints);
+
+  markerPoints.addTo(map);
+})
+
+
 
 document.getElementsByName('map').forEach(element => element.onclick = (event) => {
   map.removeLayer(activeLayer)
   activeLayer = createLayer(event.target.value).addTo(map)
+  map.removeLayer(markerPoints)
+  markerPoints = L.layerGroup();
+  feature(activeLayer.options.layers).then(response => {
+    L.geoJson(response, {
+      onEachFeature: function (feature, url) {
+        var popupOptions = { maxWidth: 250 };
+        url.bindPopup(
+          "<br><b>name: </b>" + feature.properties.name +
+          "<br><b>osm_id: </b>" + feature.properties.osm_id
+          , popupOptions);
+      }, pointToLayer: function (feature, latlng) {
+        return getMarker(feature, latlng);
+      }
+    }).addTo(markerPoints);
+
+    markerPoints.addTo(map);
+  })
 })
 
 
-map.addEventListener("click", (event) => console.log(activeLayer))
+  // var legend = L.control({ position: 'bottomleft' });
+  // legend.onAdd = function (map) {
+  //   var div = L.DomUtil.create('div', 'legend');
+  //   var labels = ["Roads", "River", "Buildings", "Cafe", "Fast food", "Restaurant", "Gas station", "Bank", "School", "Historic", "Power tower", "Bus station", "Hotel", "Other"];
+  //   var colors = ["#00F", "#00e7ef", "#858585", "#f3a6b2", "#c9151b", "#8d5a99", "#0dc900", "#ecf710", "#e8718d", "#becf50", "#987db7", "#729b6f", "#e77148", "#ff7800"];
+  //   div.innerHTML = '<div><b>Legend</b></div>';
+  //   div.style.backgroundColor = '#FFF';
+  //   div.style.padding = '12px';
+  //   for (let i = 0; i < labels.length; i++) {
+  //     div.innerHTML += '<i style="background:' + colors[i] + '">&nbsp;&nbsp;</i>&nbsp;&nbsp;'
+  //       + '<i>' + labels[i] + '<i><br/>';
+  //   }
+  //   return div;
+  // };
+  // legend.addTo(map);
 
 
 
-feature(map)("http://localhost:8080/geoserver/gis/ows")("gis:gis_osm_places_a_free_1").then(res => console.log(res))
+
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,5 @@
 import L from "leaflet";
-import $ from "jquery"
-
+import $ from 'jquery';
 
 const url = 'http://localhost:8080/geoserver/gis/wms';
 export const create = (map) => (url) => (layers) => {
@@ -14,47 +13,34 @@ export const create = (map) => (url) => (layers) => {
     })
 }
 
-export const feature = (map) => (url) => (layers) => {
+/**
+ * 
+ * WFS REQUEST
+ */
+export const feature = (layers) => {
     return new Promise((resolve, reject) => {
-        const owsrootUrl = url || 'http://localhost:8080/geoserver/gis/ows';
+        var owsrootUrl = 'http://localhost:8080/geoserver/gis/wfs';
 
-        const defaultParameters = {
+        var defaultParameters = {
             service: 'WFS',
             version: '1.0.0',
             request: 'GetFeature',
-            typeName: 'gis:gis_osm_places_a_free_1',
+            typeName: layers,
             outputFormat: 'text/javascript',
             format_options: 'callback:getJson',
             SrsName: 'EPSG:4326'
         };
 
-        const parameters = L.Util.extend(defaultParameters);
-        const URL = owsrootUrl + L.Util.getParamString(parameters);
+        var parameters = L.Util.extend(defaultParameters);
+        var URL = owsrootUrl + L.Util.getParamString(parameters);
 
-        $.ajax({
+        var ajax = $.ajax({
             url: URL,
             dataType: 'jsonp',
             contentType: 'application/json',
             jsonpCallback: 'getJson',
             success: function (response) {
                 resolve(response)
-                console.log(response);
-                // L.geoJson(response, {
-                //   onEachFeature: function (feature, url) {
-                //     popupOptions = { maxWidth: 250 };
-                //     url.bindPopup(
-                //       "<br><b>name: </b>" + feature.properties.name +
-                //       "<br><b>osm_id: </b>" + feature.properties.osm_id +
-                //       "<br><b>address:</b> " + feature.properties.address +
-                //       "<br><b>barrier: </b>" + feature.properties.barrier +
-                //       "<br><b>highway: </b>" + feature.properties.highway +
-                //       "<br><b>other_tags: </b>" + feature.properties.other_tags +
-                //       "<br><b>place: </b>" + feature.properties.place
-                //       , popupOptions);
-                //   }, pointToLayer: function (feature, latlng) {
-                //     return getMarker(feature, latlng);
-                //   }
-                // }).addTo(markerPoints);
             }
         })
     })
